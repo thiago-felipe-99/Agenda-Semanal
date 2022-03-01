544b5d966d497a6263a2fc1ab7d93414529b77e4
Thiago Felipe <thiago.felipe@tutanota.com>
2022-03-01 08:29:53 -0400

    Inicializando o projeto

---
+++ backend/controladores.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+)
+
+type controlador struct{}
+
+func (controlador *controlador) pegarID(c *gin.Context) {
+
+}
+
+func (controlador *controlador) pegarBodyTarefa(c *gin.Context) {
+
+}
+
+func (controlador *controlador) adicionarTarefa(c *gin.Context) {
+
+}
+
+func (controlador *controlador) atualizarTarefa(c *gin.Context) {
+
+}
+
+func (controlador *controlador) pegarTarefa(c *gin.Context) {
+
+}
+
+func (controlador *controlador) pegarTarefasPorDia(c *gin.Context) {
+
+}
+
+func (controlador *controlador) pegarTarefas(c *gin.Context) {
+
+}
+
+func (controlador *controlador) deletarTarefa(c *gin.Context) {
+
+}
+
+func rotasTarefas(roteamento *gin.RouterGroup, controlador *controlador) {
+	roteamento.POST("", controlador.pegarBodyTarefa, controlador.adicionarTarefa)
+	roteamento.PUT("/:id", controlador.pegarID, controlador.pegarBodyTarefa, controlador.atualizarTarefa)
+	roteamento.GET("/:id", controlador.pegarID, controlador.pegarTarefa)
+	roteamento.GET("/dia/:dia", controlador.pegarTarefasPorDia)
+	roteamento.GET("", controlador.pegarTarefas)
+	roteamento.DELETE("/:id", controlador.pegarID, controlador.deletarTarefa)
+}
+
+func rotas(url string) {
+	roteamento := gin.Default()
+
+	rotasTarefas(roteamento.Group("/atividade"), &controlador{})
+
+	if err := roteamento.Run(url); err != nil {
+		panic(err)
+	}
+}
---
+++ backend/main.go
@@ -0,0 +1,5 @@
+package main
+
+func main() {
+	rotas("127.0.0.1:8080")
+}
